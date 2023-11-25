646c58d9b23b022ff14465f1dcea86aca65ad93c
parth <pgera326@gmail.com>
2023-11-25 09:30:36 +0530

    edited chat.jsx and added socket in client

--- client/src/Components/Chat/Chat.jsx
+++ client/src/Components/Chat/Chat.jsx
@@ -1,8 +1,37 @@
+/* eslint-disable react/prop-types */
+
+import { useState, useEffect } from "react";
+import queryString from "query-string";
+import { useLocation } from "react-router-dom";
+import io from "socket.io-client";
+
+let socket;
 
 const Chat = () => {
-  return (
-    <div>Chat</div>
-  )
-}
+  const location = useLocation();
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT = "localhost:3000";
 
-export default Chat
+  console.log(name,room)
+
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
+  return <div>Chat</div>;
+};
+
+export default Chat;
--- server/index.js
+++ server/index.js
@@ -2,21 +2,27 @@ const express = require('express')
 const app = express()
 const socketio = require('socket.io')
 const http = require('http')
-// const cors = require('cors')
+const cors = require('cors')
 const router = require('./router')
 
-// app.use(cors())
+app.use(cors())
 
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer(app)
-const io = socketio(server)
+const io = socketio(server,{cors:{origin:'http://localhost:5173'}})
+
 
 
 io.on('connection', (socket)=>{
     console.log("we have new connection")
     
-    socket.on('discconet',()=>{
+    socket.on('join',({name,room},callback)=>{
+        console.log(name,room)
+        
+    })
+
+    socket.on('disconnect',()=>{
         console.log("user has left")
     })
 })
